e0489e58a9182cde2e9569aa3c8200d136ddd0c9
ricanomae6 <ricanoame6@gmail.com>
2022-05-05 04:47:09 +0900

    a

--- Bakjoon/src/Main.java
+++ Bakjoon/src/Main.java
@@ -1,60 +1,81 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
-public class Main 
-{
-	ArrayList<Integer> list = new ArrayList<Integer>();
-	int max;
+public class Main {
 	public Main() {
-		for(int i = 0; i < 9; i++) {
-			Scanner scan = new Scanner(System.in);
-			list.add(scan.nextInt());
-		}
-		for(int j = 0; j < list.size(); j++) {
-			if(j  == 0) {
-				max = list.get(j);
-			}
-			else if(list.get(j)> list.get(j-1)) {
-				max = list.get(j);
-			} 
-		}
-		System.out.println(max + "\n" + (list.indexOf(max) + 1));	
-	}		
-	public static void main(String[] args) {
-		Main main = new Main();
-	}
-}
-*/
-
-public class Main{
-	int max;
-	int where;
-	
-	int[] list = new int[9];
-	public Main() {
+		int a0 = 0;
+		int a1 = 0;
+		int a2 = 0;
+		int a3 = 0;
+		int a4 = 0;
+		int a5 = 0;
+		int a6 = 0;
+		int a7 = 0;
+		int a8 = 0;
+		int a9 = 0;
 		Scanner scan = new Scanner(System.in);
-		for(int i = 0; i < 9; i++) {
-			list[i] = scan.nextInt(); 
-		}
-		for(int j = 0; j < 9; j++) {
-			if(j==0) {
-				max = list[j];
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int d = a*b*c;
+		String sd = Integer.toString(d); //d 값을 스트링으로
+		//d = 17037300 -> 0 3번 1 1번 ...
+		int d_len = sd.length(); //d의 길이
+		for(int i = d_len - 1; i >=0; i--) { //역for문
+			switch(Character.getNumericValue(sd.charAt(i))) {
+				case 0 :
+					++a0;
+					break;
+				case 1:
+					++a1;
+					break;
+				case 2:
+					++a2;
+					break;
+				case 3:
+					++a3;
+					break;
+				case 4:
+					++a4;
+					break;
+				case 5:
+					++a5;
+					break;
+				case 6:
+					++a6;
+					break;
+				case 7:
+					++a7;
+					break;
+				case 8:
+					++a8;
+					break;
+				case 9:
+					++a9;
+					break;	
 			}
-			else if(list[j] > max) {
-				max = list[j];
-			}
 		}
-		for(int k = 0; k < list.length; k++) {
-			if(list[k] == max) {
-				where = k+1;
-			}
-		}
-		System.out.println(max + "\n" + where);
+		System.out.println(a0);
+
+		System.out.println(a1);
+
+		System.out.println(a2);
+
+		System.out.println(a3);
+
+		System.out.println(a4);
+
+		System.out.println(a5);
+
+		System.out.println(a6);
+
+		System.out.println(a7);
+
+		System.out.println(a8);
+
+		System.out.println(a9);
 	}
 	public static void main(String[] args) {
 		Main main = new Main();
 	}
-}
-
+}	
 
