51b0b1852ccee03c796a3cbc0e1d03a5b52e5e4e
sjp1798 <98084960+ricanoame6@users.noreply.github.com>
2022-05-07 22:41:41 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.HashSet;
+//import java.util.Iterator;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
